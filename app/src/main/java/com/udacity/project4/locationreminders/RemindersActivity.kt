f1b59af2e8a60edf880a559fbcac3b1e978cde4e
Fernando Renteria <klunx316@hotmail.com>
2023-10-16 14:20:36 +1100

    Permissions flow

--- app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
@@ -1,17 +1,29 @@
 package com.udacity.project4.locationreminders
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import com.udacity.project4.databinding.ActivityRemindersBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 /**
  * The RemindersActivity that holds the reminders fragments
  */
 class RemindersActivity : AppCompatActivity() {
 
+    companion object {
+        val TAG = RemindersActivity::class.java.simpleName
+        private const val REQUEST_FOREGROUND_AND_BACKGROUND_PERMISSION_RESULT_CODE = 33
+        private const val LOCATION_PERMISSION_INDEX = 0
+        private const val BACKGROUND_LOCATION_PERMISSION_INDEX = 1
+    }
+
     private lateinit var binding: ActivityRemindersBinding
+    private val sharedViewModel by viewModel<ReminderViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,5 +39,27 @@ class RemindersActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        Log.d(TAG, "onRequestPermissionResult")
+
+        if (
+            grantResults.isEmpty() ||
+            grantResults[LOCATION_PERMISSION_INDEX] == PackageManager.PERMISSION_DENIED ||
+            (requestCode == REQUEST_FOREGROUND_AND_BACKGROUND_PERMISSION_RESULT_CODE &&
+                    grantResults[BACKGROUND_LOCATION_PERMISSION_INDEX] ==
+                    PackageManager.PERMISSION_DENIED)
+        ) {
+            // Permission denied.
+            sharedViewModel.locationPermissionIsGranted(false)
+        } else {
+            sharedViewModel.locationPermissionIsGranted(true)
+        }
     }
 }
